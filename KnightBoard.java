86887bf5ff723c7f827be0246d7bc5a093ea47de
tanzimelahi <telahi00@stuy.edu>
2019-02-22 08:42:39 -0500

    beta solve

--- KnightBoard.java
+++ KnightBoard.java
@@ -12,6 +12,36 @@ public  class  KnightBoard{
      }
    }
  }
+ private boolean solveH(int row,int col,int moveNumber) {
+ System.out.println(moveNumber);
+   System.out.println("this is row:"+ row + "this is col:" + col);
+
+   if(row<0 || col<0) {
+    return false;
+  }
+  else if(row>=board.length || col>=board[0].length) {
+    return false;
+  }
+  else if(moveNumber==board.length* board[0].length) {
+   // System.out.println("this is row:"+ row + "this is col:" + col);
+    board[row][col]=moveNumber;;
+    System.out.println(board.length* board[0].length);
+    return true;
+  }
+  board[row][col]=moveNumber;
+  if(solveH(row+2,col+1,moveNumber+1)||
+   solveH(row-2,col+1,moveNumber+1)||
+   solveH(row+2,col-1,moveNumber+1)||
+   solveH(row+1,col+2,moveNumber+1)||
+   solveH(row-1,col+2,moveNumber+1)||
+   solveH(row-2,col-1,moveNumber+1)||
+   solveH(row+1,col-2,moveNumber+1)||
+   solveH(row-1,col-2,moveNumber+1)) {
+
+   return true;
+  }
+  return false;
+}
  public String toString(){
    String result=Arrays.deepToString(board);
    return result;
